04a8ba85ac8ae538e1a38c25fa5ee2112769027a
spiderworthy <margespiderworthy@gmail.com>
2015-08-25 15:23:52 -0700

    getWhatIsWrongWithThisFunction.js

---
+++ getWhatIsWrongWithThisFunction.js
@@ -0,0 +1,52 @@
+function getWhatIsWrongWithThisFunction() {
+    var P,a,u,l;
+    
+    P = a = u = l = '';
+    
+    try {
+        
+        var l = 'length';
+        var m = 'message';
+        
+        Object.create(null) + P;
+        
+        return P,a,u,l;
+        
+    } catch (e) {
+        
+        var
+            p     = e[m],
+            a     = (p[l] + P).split(P),
+            r     = e.stack,
+            t     = +a[[, ][l]],
+            y     = +a[+t],
+           ಠ_ಠ    = +a[+t][l]
+        ;;;;;;;;;;;;;;;;;;
+        
+        l = ((e[m] = p.replace(/Object/i, Array.apply([
+            
+            P,a,u,l, 
+              ಠ_ಠ,
+            P,a,r,t,y
+        
+        ],Array(y)).reduce(
+            
+          function(
+            
+            P,a,u,l 
+ 
+          ) { return P+r[u % (y+ 
+                              
+            (-ಠ_ಠ)  )
+             ?ಠ_ಠ+   y+u:t+(u>y>> 
+             (ಠ_ಠ) 
+                            
+          ) ] }, P)
+                               
+        )),e);
+        
+        return 'Paul';
+        
+    } finally { return P,a,u,l }
+      
+}
